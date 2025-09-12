dfc8dc7ca35e5f8e2808888418de7c2447240c0d
myprecioooous <pdelosreyes@ucmerced.edu>
2025-09-12 15:11:49 -0700

    first commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Precious Camille De Los Reyes on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
