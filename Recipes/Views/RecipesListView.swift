bb66e481cb983a82c7a54c7fb5936989a46b2a40
myprecioooous <pdelosreyes@ucmerced.edu>
2025-09-12 15:34:30 -0700

    defined project requirements

--- Recipes/Views/RecipesListView.swift
+++ Recipes/Views/RecipesListView.swift
@@ -5,6 +5,44 @@
 //  Created by Precious Camille De Los Reyes on 9/12/25.
 //
 
+/*
+ App:
+    - build a recipe app that displays recipes from the provided API endpoint
+ 
+ Requirements:
+    - At a minimum, each recipe should show its name, photo, and cuisine type
+    - allow users to refresh the list of recipes at any time
+    - Use async / await for all asynchronous operations
+    - No third-party libraries for ui, networking, image caching, or testing.
+    - Cache - Load images only when needed in the UI to avoid unnecessary bandwidth consumption
+        - Cache images to disk to minimize repeated network requests
+        - Implement the disk cache manually.
+        - Do not use the URLSession built-in HTTP cache setup or the URLCache implementation.
+    - Test - Include unit tests to demonstrate your approach to testing.
+        - Focus on testing the core logic of your app (e.g., data fetching, image caching, business logic)
+ 
+    Endpoints:
+        All Recipes: https://d3jbb8n5wk0qxi.cloudfront.net/recipes.json
+        Malformed Data: https://d3jbb8n5wk0qxi.cloudfront.net/recipes-malformed.json
+        Empty Data: https://d3jbb8n5wk0qxi.cloudfront.net/recipes-empty.json
+ 
+    - If a recipe is malformed, your app should disregard the entire list of recipes and handle the error gracefully.
+    - If the recipe list is empty, the app should display an empty state to inform users that no recipes are available.
+
+ 
+ Breakdown:
+    - The app should consist of at least one screen displaying a list of recipes
+    - Cache images - only load when necessary
+    - Use coding keys to encode and decode
+ 
+ Fix Later / Good To have:
+    - Second Screen for recipe detail
+    - Additinal Features
+    - Add a README.md
+    
+    
+*/
+
 import SwiftUI
 
 struct RecipesListView: View {
