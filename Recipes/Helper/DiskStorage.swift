fc15c7826fb8b751f8960807074fea4955295e05
myprecioooous <pdelosreyes@ucmerced.edu>
2025-09-16 16:03:43 -0700

    implemented DiskStorage as a helper so that app can save to disk

---
+++ Recipes/Helper/DiskStorage.swift
@@ -0,0 +1,40 @@
+//
+//  DiskStorage.swift
+//  Recipes
+//
+//  Created by Precious Camille De Los Reyes on 9/16/25.
+//
+
+import Foundation
+
+final class DiskStorage {
+    private let fileManager = FileManager.default
+    private let directory: URL
+    
+    init(subDirectory: String = "RecipeImageCache") {
+        let base = fileManager.urls(for: .cachesDirectory, in: .userDomainMask)[0]
+        self.directory = base.appendingPathComponent(subDirectory)
+        
+        //create directory at path if it doesn't exist
+        if !fileManager.fileExists(atPath: directory.path) {
+            try? fileManager.createDirectory(at: directory, withIntermediateDirectories: true, attributes: nil)
+        }
+    }
+    
+    //save
+    func save(_ data: Data, to fileName: String) throws {
+        let url = fileURL(for: fileName)
+        try data.write(to: url)
+    }
+    
+    //load
+    func load(from fileName: String) -> Data? {
+        let url = fileURL(for: fileName)
+        return try? Data(contentsOf: url)
+    }
+    
+    //get url
+    func fileURL(for fileName: String) -> URL {
+        return directory.appendingPathComponent(fileName)
+    }
+}
