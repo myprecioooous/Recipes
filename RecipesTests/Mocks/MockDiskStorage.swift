6497335c4b3e35cbcb29c1dfee1912fb0293ccde
myprecioooous <pdelosreyes@ucmerced.edu>
2025-09-21 21:08:32 -0700

    added mock for disk storage

--- RecipesTests/Mocks/MockDiskStorage.swift
+++ RecipesTests/Mocks/MockDiskStorage.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+@testable import Recipes
+
+final class MockDiskStorage: DiskStoring {
+    private var store: [String: Data] = [:]
+    
+    func save(_ data: Data, to fileName: String) throws {
+        store[fileName] = data
+    }
+    
+    func load(from fileName: String) -> Data? {
+        store[fileName]
+    }
+    
+    //get url
+    private func fileURL(for fileName: String) -> URL {
+        return URL(fileURLWithPath: "/mock/path/\(fileName)")
+    }
+    
+    
+}
