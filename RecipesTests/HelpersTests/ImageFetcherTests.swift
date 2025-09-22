8f11c30b5cffa2ecda0f61edc6dabb0cffba2cab
myprecioooous <pdelosreyes@ucmerced.edu>
2025-09-21 21:34:09 -0700

    added tests for image fetcher

--- RecipesTests/HelpersTests/ImageFetcherTests.swift
+++ RecipesTests/HelpersTests/ImageFetcherTests.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import XCTest
+
+@testable import Recipes
+
+final class ImageFetcherTests: XCTestCase {
+    func testFetchImageFromCache() async {
+        //Given
+        let url = URL(string: "https://example.com/test.png")!
+        let expectedImage = UIImage(systemName: "photo")!
+        let mockCache = MockImageCache()
+        mockCache.storedImages[url] = expectedImage
+        
+        //can just use ImageFetcher directly since it's just a wrapper anyway for ImageCache
+        let fetcher = ImageFetcher(cache: mockCache)
+        
+        //When
+        let image = await fetcher.fetchImage(from: url.absoluteString)
+        
+        //Assert
+        XCTAssertEqual(image, expectedImage)
+    }
+}
