d3b2d9a01b73c4fd8ba7c42eb22366e1b046b653
myprecioooous <pdelosreyes@ucmerced.edu>
2025-09-21 21:58:09 -0700

    added tests for image cache test

--- RecipesTests/HelpersTests/ImageCacheTests.swift
+++ RecipesTests/HelpersTests/ImageCacheTests.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+import XCTest
+
+@testable import Recipes
+
+final class ImageCacheTests: XCTestCase {
+    func testLoadFromDiskCache() async {
+        //Given
+        let mockDisk = MockDiskStorage()
+        let cache = ImageCache(diskStorage: mockDisk)
+        
+        let data = UIImage(systemName: "photo")!.pngData()!
+        try? mockDisk.save(data, to: "https://example.com/image.png".sha256)
+        
+        let url = URL(string: "https://example.com/image.png")!
+        
+        //When
+        let loadedImage = await cache.image(for: url)
+        
+        //Then
+        XCTAssertNotNil(loadedImage)
+        
+    }
+}
