cec9c1fd9d88477b22ae39b5edc3fb49967983fc
myprecioooous <pdelosreyes@ucmerced.edu>
2025-09-21 22:15:08 -0700

    added tests for disk storage test

--- RecipesTests/HelpersTests/DiskStorageTests.swift
+++ RecipesTests/HelpersTests/DiskStorageTests.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+import XCTest
+
+@testable import Recipes
+
+final class DiskStorageTests: XCTestCase {
+    func testSaveAndLoadData() throws {
+        //Given
+        let mockDisk = MockDiskStorage()
+        let data = "Hello World".data(using: .utf8)!
+        
+        //When
+        try mockDisk.save(data, to: "testfile")
+        let loadedData = mockDisk.load(from: "testfile")
+        
+        //Then
+        XCTAssertNotNil(loadedData)
+        XCTAssertEqual(loadedData, data)
+        
+    }
+}
