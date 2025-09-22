ba08a46d474b146f07d676613a2665249cfe1732
myprecioooous <pdelosreyes@ucmerced.edu>
2025-09-21 21:02:59 -0700

    added mock for image cache

--- RecipesTests/Mocks/MockImageCache.swift
+++ RecipesTests/Mocks/MockImageCache.swift
@@ -5,4 +5,17 @@
 //  Created by Precious Camille De Los Reyes on 9/19/25.
 //
 
-import Foundation
+import SwiftUI
+@testable import Recipes
+
+final class MockImageCache: ImageCaching {
+    
+    var storedImages: [URL:UIImage] = [:]
+    var shouldReturnNil = false
+    
+    func image(for url: URL) async -> UIImage? {
+        if shouldReturnNil { return nil }
+        return storedImages[url]
+    }
+
+}
